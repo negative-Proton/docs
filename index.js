0bbce95eca48608222ea689d1b497d889cec9ae9
negative-Proton <pai.shreya1499@gmail.com>
2022-04-23 14:20:38 +0530

    clashing commit after container adding

--- index.js
+++ index.js
@@ -11,3 +11,4 @@ console.log('2nd commit from RB-1.0.0.1');
 console.log('3rd Commit from RB-1.0.0.1');
 console.log('4th commit from RB-1.0.0.1');
 console.log('5th commit from bpai2');
+console.log('6th commit from bpai2 after a non clashing file');
